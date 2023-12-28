1f5b9ed6c246de97195412905de8eb812bfbc92c
edndacomputer <edndacomputer@riseup.net>
2023-12-28 00:57:39 -0800

    not-found

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import { Button } from "@/app/components/ui/button";
 import HeaderElement from "./HeaderElement"; // adjust the path according to your project structure
 import SocialIcons from "@/app/components/social-links";
+import Image from "next/image";
 
 const HeaderComponent = () => {
   return (
     <section>
       <div
-        className="relative overflow-hidden bg-cover bg-no-repeat"
+        className="relative overflow-hidden bg-cover bg-no-repeat rounded-xl"
         style={{
           backgroundPosition: "50%",
-          backgroundImage:
-            "url('https://tecdn.b-cdn.net/img/new/slides/146.webp')",
           height: "450px",
         }}
       >
+        <Image
+          src="https://tecdn.b-cdn.net/img/new/slides/146.webp"
+          layout="fill"
+          objectFit="cover"
+          alt="Background Image"
+        />
         <div
           className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.75)" }}
